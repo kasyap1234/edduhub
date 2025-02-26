5531e52c5eed0bb5ba45b45b911e67a17ca66283
tgt <kasyap3103@gmail.com>
2025-02-26 16:36:53 +0530

    auth

--- server/internal/auth/auth_service.go
+++ server/internal/auth/auth_service.go
@@ -1,9 +1,21 @@
 package auth
 
-import "github.com/go-chi/chi"
+import (
+	"github.com/go-chi/chi"
+	"github.com/kasyap1234/eduhub-backend/server/internal/config"
+	"github.com/kasyap1234/eduhub-backend/server/internal/repository"
+	"github.com/supertokens/supertokens-golang/recipe/emailpassword"
+)
 
-type AuthHandler struct {
-	router chi.Router
-	jwtSecret []byte 
-	userStore UserStore
+type SuperTokensAuth struct {
+	config *config.SuperTokensConfig
+	userRepo repository.UserRepository
 }
+
+func NewAuthService(config *config.SuperTokensConfig,userRepo repository.UserRepository)*SuperTokensAuth{
+	return &SuperTokensAuth{
+		config: config, 
+		userRepo : userRepo,
+	}
+}
+
---
+++ server/internal/auth/interfaces.go
@@ -0,0 +1,17 @@
+package auth
+
+
+// import "github.com/kasyap1234/eduhub-backend/server/internal/database/models"
+
+// type AuthService interface {
+// 	Login(email, password string) (*Session, error)
+// 	Register(user *models.User) error 
+// 	ValidateSession(token string)(*Session,error)
+// 	GetUserRole(userID string)(string,error)
+// }
+
+// type Session struct {
+// 	UserID string 
+// 	Role string 
+// 	Token string 
+// }
