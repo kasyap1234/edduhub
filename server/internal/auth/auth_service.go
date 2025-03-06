a7d23bd4c74c0757a308471cc1637b7cc1da9394
tgt <kasyap3103@gmail.com>
2025-03-06 10:22:31 +0530

    auth service init

--- server/internal/auth/auth_service.go
+++ server/internal/auth/auth_service.go
@@ -1,2 +1,25 @@
 package auth
 
+import (
+	"context"
+	"github.com/zitadel/zitadel-go/v3/pkg/client/zitadel"
+	"eduhub/server/internal/config"
+)
+
+type AuthService struct {
+	client *zitadel.Client
+	authConfig *config.AuthConfig
+
+}
+
+
+func NewAuthService(cfg *config.AuthConfig)*AuthService{
+	return &AuthService{
+		client : cfg.Client, 
+		authConfig: cfg,
+	}
+}
+
+func(s *AuthService)GetLoginURL()string {
+	
+}
