9c10bd94bdd9ead7c9d3aace139918c98e2956e4
tgt <kasyap3103@gmail.com>
2025-03-17 11:42:28 +0530

    main

--- server/internal/repository/database_repository.go
+++ server/internal/repository/database_repository.go
@@ -1,1 +1,36 @@
 package repository
+
+import (
+	"context"
+	"github.com/uptrace/bun"
+)
+
+type DatabaseRepository[T any] interface {
+	Create(ctx context.Context, model *T) error
+	CreateMany(ctx context.Context, models []*T) error
+	FindByID(ctx context.Context, id interface{}) (*T, error)
+	FindOne(ctx context.Context, query string, args ...interface{}) (*T, error)
+	FindAll(ctx context.Context) ([]*T, error)
+	FindWhere(ctx context.Context, query string, args ...interface{}) ([]*T, error)
+	Update(ctx context.Context, model *T) error
+	Delete(ctx context.Context, model *T) error
+	DeleteByID(ctx context.Context, id interface{}) error
+	Count(ctx context.Context, query string, args ...interface{}) (int, error)
+	// transaction
+}
+
+type BaseDatabaseRepository[T any] struct {
+	DB *bun.DB
+}
+
+func NewBaseRepository[T any](db *bun.DB)*BaseDatabaseRepository[T]{
+	return &BaseDatabaseRepository[T]{
+		DB : db,
+	}
+}
+
+func(d *BaseDatabaseRepository[T])Create(ctx context.Context,model *T)error {
+ _,err :=d.DB.NewInsert().Model(model).Exec(ctx)
+   return err 
+}
+
