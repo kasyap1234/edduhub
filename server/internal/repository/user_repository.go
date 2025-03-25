ce5d8263c028659c159bf21bc4f1cf2bf7930643
tgt <kasyap3103@gmail.com>
2025-03-25 12:42:42 +0530

    db interface

--- server/internal/repository/user_repository.go
+++ server/internal/repository/user_repository.go
@@ -9,34 +9,39 @@ import (
 
 type UserRepository interface {
 	CreateUser(ctx context.Context, user *models.User) error
-	FindByRollNo(ctx context.Context, RollNo string) *models.User
-	UpdateUser(ctx context.Context, user *models.User) *models.User
+	FindByRollNo(ctx context.Context, RollNo string) (*models.User,error)
+	UpdateUser(ctx context.Context, user *models.User)( *models.User,error)
 	FreezeUser(ctx context.Context, RollNo string) error
 	DeleteUser(ctx context.Context, RollNo string) error
 }
 
 type userRepository struct {
-	db *bun.DB
+	db  DatabaseRepository[models.User]
 }
 
-func NewUserRepository(db bun.DB) UserRepository {
+func NewUserRepository(db DatabaseRepository[models.User]) UserRepository {
 	return &userRepository{
-		db: &db,
+		db: db ,
 	}
 }
 
 func(u*userRepository)CreateUser(ctx context.Context,user*models.User)error {
-	userModel :=new(models.User)
-	_,err :=u.db.NewInsert().Model(userModel).Exec(ctx)
-	return err 
+	return u.db.Create(ctx,user)
 }
 
-func(u*userRepository)FindByRollNo(ctx context.Context,RollNo string)*models.User{
-	user :=new(models.User)
-	err :=u.db.NewSelect().Model(user).Where("roll_no = ?",RollNo).Scan(ctx)
-	if err !=nil {
-		return nil 
-	}
-	return user 
+func(u*userRepository)FindByRollNo(ctx context.Context,RollNo string)(*models.User,error){
+	return u.db.FindOne(ctx,"roll_no=?",RollNo)
+
 }
 
+func(u*userRepository)DeleteUser(ctx context.Context,RollNo string)error{
+
+}
+
+func(u*userRepository)FreezeUser(ctx context.Context,RollNo string)error {
+
+}
+
+func(u*userRepository)UpdateUser(ctx context.Context,model*models.User)(*models.User,error){
+
+}
