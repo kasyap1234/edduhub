a79421ae54190930a75e6064c09ac8178e9b5e8e
Kasyap Dharanikota <kasyap3103@gmail.com>
2025-03-31 11:21:28 +0530

    attendance-repo

--- server/internal/repository/attendance_repository.go
+++ server/internal/repository/attendance_repository.go
@@ -6,7 +6,7 @@ import (
 )
 
 type AttendanceRepository interface {
-	MarkAttendance(ctx context.Context, studentID int, courseID int, lectureID int) error
+	MarkAttendance(ctx context.Context, studentID int, courseID int, lectureID int) (bool,error)
 	UpdateAttendance(ctx context.Context, studentID int, courseID int, lectureID int, status string) error
 	GetAttendanceStudentInCourse(ctx context.Context, studentID int, courseID int) ([]*models.Attendance, error)
 	GetAttendanceStudent(ctx context.Context, studentID int) ([]*models.Attendance, error)
@@ -22,17 +22,22 @@ func NewAttendanceRepository(db DatabaseRepository[models.Attendance]) Attendanc
 		db: db,
 	}
 }
+// mark attendance 
 
-func (a *attendanceRepository) MarkAttendance(ctx context.Context, studentID int, courseID int, lectureID int) error {
+func (a *attendanceRepository) MarkAttendance(ctx context.Context, studentID int, courseID int, lectureID int)(bool,error) {
 
 	attendance := &models.Attendance{
 		StudentID: studentID,
 		CourseID:  courseID,
 		LectureID: lectureID,
 	}
-	return a.db.Create(ctx, attendance)
+	err := a.db.Create(ctx, attendance)
+	if err !=nil {
+		return false,err 
+	}
+	return true, nil 
 }
-
+// to update student attendance for a single lecture in a course 
 func (a *attendanceRepository) UpdateAttendance(ctx context.Context, studentID int, courseID int, lectureID int, status string) error {
 	attendance, err := a.db.FindOne(ctx, "student_id = ? AND course_id=? AND lecture_id=?", studentID, courseID, lectureID)
 	if err != nil {
@@ -42,6 +47,9 @@ func (a *attendanceRepository) UpdateAttendance(ctx context.Context, studentID i
 	return a.db.Update(ctx, attendance)
 
 }
+
+// to get attendance of student in a course a course is a series of lectures wil give student attendance for the entire course for example student 1 has attendance for lecture 1 and lecture2 of course 1 
+
 func (a *attendanceRepository) GetAttendanceStudentInCourse(ctx context.Context, studentID int, courseID int) ([]*models.Attendance, error) {
 
 	attendances, err := a.db.FindWhere(ctx, "student_id=? AND course_id=?", studentID, courseID)
@@ -50,6 +58,8 @@ func (a *attendanceRepository) GetAttendanceStudentInCourse(ctx context.Context,
 	}
 	return attendances, nil
 }
+// to get attendance of student across all courses 
+// to get attendance of student across all lectures
 
 func (a *attendanceRepository) GetAttendanceStudent(ctx context.Context, studentID int) ([]*models.Attendance, error) {
 	records, err := a.db.FindWhere(ctx, "student_id = ?", studentID)
--- server/internal/services/attendance/attendance_service.go
+++ server/internal/services/attendance/attendance_service.go
@@ -12,8 +12,8 @@ type AttendanceService interface {
 	GenerateQRCode(courseID int, lectureID int) (string, error)
 	GetAttendanceByLecture(courseID int, lectureID int) ([]*models.Attendance, error)
 	GetAttendanceByCourse(courseID int) (*models.Attendance, error)
-	GetAttendanceByStudent(studentID int) (*models.Attendance, error)
-	GetAttendanceByStudentAndCourse(studentID int, courseID int) (*models.Attendance, error)
+	GetAttendanceByStudent(studentID int) ([]*models.Attendance, error)
+	GetAttendanceByStudentAndCourse(studentID int, courseID int) ([]*models.Attendance, error)
 	MarkAttendance(studentID int, courseID int, lectureID int) (bool, error)
 }
 
@@ -23,26 +23,33 @@ type attendanceService struct {
 
 func NewAttendanceService(repo repository.AttendanceRepository) AttendanceService {
 	return &attendanceService{
-	repo :repo ,
+		repo: repo,
 	}
 }
 
 func (a *attendanceService) GetAttendanceByLecture(courseID int, lectureID int) ([]*models.Attendance, error) {
-return a.repo.GetAttendanceByLecture(context.Background(),courseID,lectureID)
+	return a.repo.GetAttendanceByLecture(context.Background(), courseID, lectureID)
 }
 
 func (a *attendanceService) GetAttendanceByCourse(courseID int) (*models.Attendance, error) {
 
 }
 
-func (a *attendanceService) GetAttendanceByStudent(studentID int) (*models.Attendance, error) {
-
+func (a *attendanceService) GetAttendanceByStudent(studentID int) ([]*models.Attendance, error) {
+	return a.repo.GetAttendanceStudent(context.Background(), studentID)
 }
 
-func (a *attendanceService) GetAttendanceByStudentAndCourse(studentID int, courseID int) (*models.Attendance, error) {
-
+func (a *attendanceService) GetAttendanceByStudentAndCourse(studentID int, courseID int) ([]*models.Attendance, error) {
+	return a.repo.GetAttendanceStudentInCourse(context.Background(), studentID, courseID)
 }
 
 func (a *attendanceService) MarkAttendance(studentID, courseID, lectureID int) (bool, error) {
-
+	ok, err := a.repo.MarkAttendance(context.Background(), studentID, courseID, lectureID)
+	if err != nil {
+		return false, err
+	}
+	if ok {
+		return true, nil
+	}
+	return false, nil
 }
