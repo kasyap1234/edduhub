bd466c210de532ea56d25ef372702edf47c4a95d
Kasyap Dharanikota <kasyap3103@gmail.com>
2025-04-08 18:29:26 +0530

    add  success handler

--- server/internal/helpers/success.go
+++ server/internal/helpers/success.go
@@ -1,1 +1,15 @@
 package helpers
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type SuccessResponse struct {
+	Data any `json:"data"`
+}
+
+func Success(c echo.Context, data any) error {
+	return c.JSON(http.StatusOK, SuccessResponse{Data: data})
+}
