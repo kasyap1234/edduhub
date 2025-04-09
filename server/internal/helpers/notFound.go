b4f25518d56ab8eaa51e0f5480b6380fa0ea8af1
tgt <kasyap3103@gmail.com>
2025-04-09 16:01:58 +0530

    add not found response

---
+++ server/internal/helpers/notFound.go
@@ -0,0 +1,18 @@
+package helpers
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type NotFoundResponse struct {
+	Data any `json:"data"`
+}
+
+
+func NotFound(c echo.Context,data any)error {
+	return c.JSON(http.StatusNotFound,NotFoundResponse{
+		Data : data,
+	}
+}
