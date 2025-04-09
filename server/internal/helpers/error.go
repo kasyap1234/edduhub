dcdd287bb242e67e1dfc5d5d33d5f04ee9f16c1b
tgt <kasyap3103@gmail.com>
2025-04-09 15:40:49 +0530

    add error and success helpers

--- server/internal/helpers/error.go
+++ server/internal/helpers/error.go
@@ -1,1 +1,17 @@
 package helpers
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type ErrorResponse struct {
+	Data any `json:"data"`
+}
+
+func Error(c echo.Context, data any) error {
+	return c.JSON(http.StatusInternalServerError, ErrorResponse{
+		Data: data,
+	})
+}
