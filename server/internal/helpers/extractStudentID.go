95f8fa019b3e6f437ce186d52f449beb7571bf64
Kasyap Dharanikota <kasyap3103@gmail.com>
2025-04-19 16:50:42 +0530

    fix error

--- server/internal/helpers/extractStudentID.go
+++ server/internal/helpers/extractStudentID.go
@@ -6,7 +6,7 @@ func ExtractStudentID(c echo.Context) (int, error) {
 	studentID := c.Get("student_id")
 	studentIDInt, ok := studentID.(int)
 	if !ok {
-		return 0, Error(c, "invalid student_id")
+		return 0, Error(c, "invalid student_id", 400)
 	}
 	return studentIDInt, nil
 }
