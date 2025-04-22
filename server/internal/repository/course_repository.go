cb7f141a430c3d49371c99a21a47fc1dcf29314a
tgt <kasyap3103@gmail.com>
2025-04-22 12:07:01 +0530

    implement CourseRepository and CreateCourse method in course_repository

--- server/internal/repository/course_repository.go
+++ server/internal/repository/course_repository.go
@@ -1,2 +1,25 @@
 package repository
 
+import (
+	"context"
+	"eduhub/server/internal/models"
+)
+
+type CourseRepository interface {
+	CreateCourse(ctx context.Context, course *models.Course) error
+}
+
+type courseRepository struct {
+	db DatabaseRepository[models.Course]
+}
+
+func NewCourseRepository(db DatabaseRepository[models.Course]) CourseRepository {
+	return &courseRepository{
+		db: db,
+	}
+}
+
+func (c *courseRepository) CreateCourse(ctx context.Context, course *models.Course) error {
+	return c.db.Create(ctx, course)
+
+}
