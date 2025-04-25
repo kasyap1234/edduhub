cae474103b760e8ec963ffd68894eafd1d148eb3
tgt <kasyap3103@gmail.com>
2025-04-25 16:26:34 +0530

    add tests

--- server/internal/repository/enrollment_repository_test.go
+++ server/internal/repository/enrollment_repository_test.go
@@ -1,7 +1,30 @@
 package repository
 
-import "testing"
+import (
+	"context"
+	"eduhub/server/internal/models"
+	"eduhub/server/mocks"
+	"testing"
+	"time"
 
-func TestCreateEnrollment(t *testing.T){
-	
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateEnrollment(t *testing.T) {
+	ctx := context.Background()
+
+	mockRepo := new(mocks.EnrollmentRepository)
+	enrollment := &models.Enrollment{
+		ID:             1,
+		StudentID:      1,
+		CourseID:       1,
+		EnrollmentDate: time.Now(),
+		Status:         "active",
+	}
+
+	mockRepo.On("CreateEnrollment", ctx, enrollment).Return(nil)
+	err := mockRepo.CreateEnrollment(ctx, enrollment)
+	assert.NoError(t, err)
+	mockRepo.AssertExpectations(t)
+
 }
