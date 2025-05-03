b43a1be73e5470d44d42af83969598002fec6ddc
Kasyap Dharanikota <kasyap3103@gmail.com>
2025-05-03 09:48:16 +0530

    add student service

--- server/internal/services/student/student_service.go
+++ server/internal/services/student/student_service.go
@@ -1,68 +1,20 @@
 package student
 
-import (
-	"context"
-
-	"eduhub/server/internal/models"
-	"eduhub/server/internal/repository"
-)
+import "eduhub/server/internal/repository"
 
 type StudentService interface {
-	CreateStudent(ctx context.Context, student *models.Student) error
-	GetStudentByRollNo(ctx context.Context, rollNo string) (*models.Student, error)
-	GetStudentByID(ctx context.Context, collegeID, studentID int) (*models.Student, error)
-	UpdateStudent(ctx context.Context, model *models.Student) error
-	FreezeStudent(ctx context.Context, RollNo string) error
-	UnFreezeStudent(ctx context.Context, RollNo string) error
-	FindByKratosID(ctx context.Context, kratosID string) (*models.Student, error)
-	VerifyStudentEnrollment(ctx context.Context, collegeID, studentID, courseID int) (bool, error)
 }
 
 type studentService struct {
-	StudentRepo    repository.StudentRepository
-	AttendanceRepo repository.AttendanceRepository
-	EnrollmentRepo repository.EnrollmentRepository
+	studentRepo    repository.StudentRepository
+	attendanceRepo repository.AttendanceRepository
+	enrollmentRepo repository.EnrollmentRepository
 }
 
-func NewstudentService(studentRepo repository.StudentRepository, attendance repository.AttendanceRepository, enrollment repository.EnrollmentRepository) StudentService {
+func NewstudentService(studentRepo repository.StudentRepository, attendanceRepo repository.AttendanceRepository, enrollmentRepo repository.EnrollmentRepository) StudentService {
 	return &studentService{
-		StudentRepo:    studentRepo,
-		AttendanceRepo: attendance,
-		EnrollmentRepo: enrollment,
+		studentRepo:    studentRepo,
+		attendanceRepo: attendanceRepo,
+		enrollmentRepo: enrollmentRepo,
 	}
-}
-
-func (s *studentService) CreateStudent(ctx context.Context, student *models.Student) error {
-	return s.StudentRepo.CreateStudent(ctx, student)
-}
-
-func (s *studentService) FindByKratosID(ctx context.Context, kratosID string) (*models.Student, error) {
-	student, err := s.StudentRepo.FindByKratosID(ctx, kratosID)
-	return student, err
-}
-
-func (s *studentService) GetStudentByRollNo(ctx context.Context, rollNo string) (*models.Student, error) {
-	student, err := s.StudentRepo.GetStudentByRollNo(ctx, rollNo)
-	return student, err
-}
-
-func (s *studentService) GetStudentByID(ctx context.Context, collegeID, studentID int) (*models.Student, error) {
-	student, err := s.StudentRepo.GetStudentByID(ctx, collegeID, studentID)
-	return student, err
-}
-
-func (s *studentService) UpdateStudent(ctx context.Context, model *models.Student) error {
-	return s.StudentRepo.UpdateStudent(ctx, model)
-}
-
-func (s *studentService) FreezeStudent(ctx context.Context, RollNo string) error {
-	return s.StudentRepo.FreezeStudent(ctx, RollNo)
-}
-
-func (s *studentService) UnFreezeStudent(ctx context.Context, RollNo string) error {
-	return s.StudentRepo.UnFreezeStudent(ctx, RollNo)
-}
-
-func (s *studentService) VerifyStudentEnrollment(ctx context.Context, collegeID, studentID, courseID int) (bool, error) {
-	return s.EnrollmentRepo.IsStudentEnrolled(ctx, collegeID, studentID, courseID)
 }
